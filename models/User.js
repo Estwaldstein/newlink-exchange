24348cb7312413a0f474b6a29fb71bc77f4395f8
Estwaldstein <andrea.toscani@mac.com>
2025-04-22 22:26:13 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['introducer', 'partner', 'admin'], default: 'introducer' }
+  role: { type: String, enum: ['introducer', 'partner', 'admin'], default: 'introducer' },
 
-notifications: [{
-  content: String,
-  read: { type: Boolean, default: false },
-  timestamp: { type: Date, default: Date.now }
-}]
+  notifications: [{
+    content: String,
+    read: { type: Boolean, default: false },
+    timestamp: { type: Date, default: Date.now }
+  }]
 });
 
 module.exports = mongoose.model('User', userSchema);
